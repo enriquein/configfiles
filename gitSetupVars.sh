cb661635e6180534c7b706f23626eff1e1b875b6
Enrique Ramirez <enriquein@gmail.com>
2011-04-23 12:22:28 -0400

    Added first time setup for git.

---
+++ gitSetupVars.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git config --global user.name "Enrique Ramirez"
+git config --global user.email enriquein@gmail.com
+git config --global color.ui auto
+git config --global core.autocrlf false
+git config --global core.safecrlf false
+
