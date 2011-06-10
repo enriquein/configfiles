24090b284221fff151a2b341337f877f72717e94
Enrique Ramirez <enriquein@gmail.com>
2011-06-10 08:54:46 -0400

    Added new goodies to git.

--- gitSetupVars.sh
+++ gitSetupVars.sh
@@ -4,4 +4,4 @@ git config --global user.email enriquein@gmail.com
 git config --global color.ui auto
 git config --global core.autocrlf false
 git config --global core.safecrlf false
-
+git config --global core.whitespace cr-at-eol
