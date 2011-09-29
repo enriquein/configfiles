023d65ecbf50acadf68cf60a7079ec5b2e394133
Enrique Ramirez <enriquein@gmail.com>
2011-09-29 18:07:06 -0400

    Changed default script to include proper autocrlf comment.

--- gitSetupVars.sh
+++ gitSetupVars.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+echo "Remember to uncomment the proper autocrlf option in this script"
 git config --global user.name "Enrique Ramirez"
 git config --global user.email enriquein@gmail.com
 git config --global color.ui auto
-git config --global core.autocrlf false
-git config --global core.safecrlf false
+#on unix boxes
+#git config --global core.autocrlf input
+#on windows boxes
+#git config --global core.autocrlf true
 git config --global core.whitespace cr-at-eol
